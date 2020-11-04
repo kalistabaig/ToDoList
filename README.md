7ed196213c1fd986370bb06b645dde93cf3e63b4
kalista <kalistaSbaig@gmail.com>
2020-11-04 15:19:23 -0500

    added readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# To Do List
+
+A simple single page web application designed for recording tasks that need to be comleted and can be assigned a degree of urgency to be completed.
+
+## Instructions
+
+1. In the text area, type in the task that needs to be added to the to do list.
+2. Using the drop down list beside the text area, select the degree of urgency the task needs 
+3. To add the new task to the list click the 'Add' button
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+const newList = document.getElementById("create-new-list");
+newList.onclick = createNewListClick();
 const addButton = document.getElementById("addButton");
 addButton.onclick = addButtonClick;
 var tasks = [];
 loadTasks();
 
-function addButtonClick(){
+function createNewListClick() {
+    const listName = document.getElementById("list-name").value;
+    addListToApi(listName, addListToUI);
+}
+
+
+function addButtonClick() {
     var newTask = {
         name: document.getElementById('todoText').value,
-        priority: 3
+        priority: document.getElementById('prioritySelector').value
     }
+
     document.getElementById('todoText').value = "";
+    document.getElementById('prioritySelector').value = 0;
     addTaskToApi(newTask, addTaskToUI);
     
    
@@ -21,12 +31,48 @@ function addTaskToUI(task){
     const listItem = document.createElement("li");
     listItem.setAttribute("data-id",task.id)
     const nameSpan = document.createElement("span");
+    nameSpan.className = "nameSpan";
     nameSpan.innerHTML = task.name;
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.className = "nameInput";
+    const prioritySelect = document.createElement("select");
+    prioritySelect.className = "prioritySelect";
+    
+
+    var i;
+    optionNames = ["None", "Low", "Medium", "High", "Critical"];
+
+    for (i = 0; i < 5; i++){
+        const option = document.createElement("option");
+        option.innerHTML = optionNames[i];
+        option.value = i;
+        prioritySelect.appendChild(option);
+    }
+    prioritySelect.value = task.priority;
+
     const deleteItem = document.createElement("button");
+    deleteItem.className = "deleteButton";
     deleteItem.onclick = deleteButtonClick;
     deleteItem.innerText = "Delete"
+
+    const editButton = document.createElement("button");
+    editButton.className = "editButton";
+    editButton.onclick = editButtonClick;
+    editButton.innerText = "Edit";
+    
+    const saveButton = document.createElement("button");
+    saveButton.className = "saveButton";
+    saveButton.onclick = saveButtonClick;
+    saveButton.innerText = "Save";
+
     listItem.appendChild(nameSpan);
+    listItem.appendChild(nameInput);
+    listItem.appendChild(prioritySelect);
     listItem.appendChild(deleteItem);
+    listItem.appendChild(editButton);
+    listItem.appendChild(saveButton);
+    
 
     document.getElementById("taskList").appendChild(listItem);
 }
@@ -42,19 +88,20 @@ function addTaskToApi(task, callback){ //request like in postman
         },
     }
     
-    fetch("http://localhost:4000/task", fetchOptions) //send button on postman, mae s actual call to server
+    fetch("http://kalistabaig.me:4000/task", fetchOptions) //send button on postman, mae s actual call to server
     .then((response) =>{
-        console.log("post status: " + response.status);
         return response.json();
     })
     .then((data) => {
         var task = data;
+        tasks.push(task);
         callback(task)
     })
+
 }
 
 function loadTasks() {
-    fetch("http://localhost:4000/task") //fetch returns a promise        by default fetch is a get
+    fetch("http://kalistabaig.me:4000/task") //fetch returns a promise        by default fetch is a get
     .then((response) => response.json())
     .then((data) => {
         tasks = data
@@ -71,7 +118,55 @@ function deleteButtonClick(){
     const fetchOptions = {
         method: "DELETE"
     }
-    fetch("http://localhost:4000/task/" + taskId, fetchOptions)
+    fetch("http://kalistabaig.me:4000/task/" + taskId, fetchOptions)
+    .then((response) => console.log("post status:" + response.status))
+
+}
+
+function editButtonClick(){
+    const nameSpan = this.parentNode.getElementsByClassName("nameSpan")[0];
+    nameSpan.style.display = "none";
+    const nameInput = this.parentNode.getElementsByClassName("nameInput")[0];
+    nameInput.style.display = "inline-block";
+    nameInput.value = nameSpan.innerHTML;
+    const editButton = this.parentNode.getElementsByClassName("editButton")[0];
+    editButton.style.display = "none";
+    const saveButton = this.parentNode.getElementsByClassName("saveButton")[0];
+    saveButton.style.display = "inline-block";
+}
+
+function saveButtonClick(){
+    const nameSpan = this.parentNode.getElementsByClassName("nameSpan")[0];
+    nameSpan.style.display = "inline-block";
+    const nameInput = this.parentNode.getElementsByClassName("nameInput")[0];
+    nameInput.style.display = "none";
+    nameSpan.innerHTML = nameInput.value;
+    const editButton = this.parentNode.getElementsByClassName("editButton")[0];
+    editButton.style.display = "inline-block";
+    const saveButton = this.parentNode.getElementsByClassName("saveButton")[0];
+    saveButton.style.display = "none";
+
+    const newPriority = this.parentNode.getElementsByClassName("prioritySelect")[0].value;
+
+    var taskId = this.parentNode.getAttribute("data-id");
+    var task = tasks.find(task => task.id == taskId);
+    if(!task){
+        console.log("Task not found");
+        return;
+    }
+    task.priority = newPriority;
+    task.name = nameSpan.innerHTML;
+
+    const fetchOptions = {
+        method: "PUT",
+        body: JSON.stringify(task),
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+    fetch("http://kalistabaig.me:4000/task/" + taskId, fetchOptions)
     .then((response) => console.log("post status:" + response.status))
 
 }
